37ce0ae2f3424c030140758f62b9ccccbb0e4d1c
Maulana Raji Shofil Fuadi <maulanarajisf@gmail.com>
2026-06-08 04:35:30 +0800

    fix(RotatingText): remove layout prop to fix animation glitch

--- app/components/RotatingText/RotatingText.tsx
+++ app/components/RotatingText/RotatingText.tsx
@@ -184,7 +184,6 @@ const RotatingText = forwardRef<RotatingTextRef, RotatingTextProps>(
       <motion.span
         className={cn('flex flex-wrap whitespace-pre-wrap relative', mainClassName)}
         {...rest}
-        layout
         transition={transition}
       >
         <span className="sr-only">{texts[currentTextIndex]}</span>
@@ -192,7 +191,6 @@ const RotatingText = forwardRef<RotatingTextRef, RotatingTextProps>(
           <motion.span
             key={currentTextIndex}
             className={cn(splitBy === 'lines' ? 'flex flex-col w-full' : 'flex flex-wrap whitespace-pre-wrap relative')}
-            layout
             aria-hidden="true"
           >
             {elements.map((wordObj, wordIndex, array) => {
